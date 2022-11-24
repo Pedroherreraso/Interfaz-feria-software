9d9b31fe736bba51f05ae864a2767322205105c1
pedro.herreraso <pedro.herreraso@sansano.usm.cl>
2022-11-24 11:08:31 -0300

    Se crea componente y se actualiza el color del bg

--- feria_sofware/src/App.vue
+++ feria_sofware/src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
+    <Pagina_Inicial/>
+  </template>
+    
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+import Pagina_Inicial from './components/Pagina_Inicial';
+export default{
+    name: 'app',
+    components: {
+        Pagina_Inicial,
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+body{
+  margin: 0; 
+  padding: 0;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #162C41;
+  min-height: 100vh;
 }
 </style>
---
+++ feria_sofware/src/components/Pagina_Inicial.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="pagina_inicial">
+        <h1>Esto es una prueba</h1>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'pagina_inicial',
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<style>
+.pagina_inicial{
+    position: absolute;
+    width: 100%;
+    text-align: center;
+}
+</style>
